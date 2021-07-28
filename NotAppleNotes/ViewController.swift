a364baf3d7d0385b6f63efb9fc323c1aa1e4dac2
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-07-28 17:11:21 +0300

    Initial Commit

---
+++ NotAppleNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotAppleNotes
+//
+//  Created by Igor Chernyshov on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
